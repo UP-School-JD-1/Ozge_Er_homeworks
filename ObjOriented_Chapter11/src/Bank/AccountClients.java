174a9eb736e19ba803cf4c1ce97406245d225f73
Özge <83001974+ozgeer@users.noreply.github.com>
2022-08-20 15:38:53 -0700

    Update AccountClients.java

--- ObjOriented_Chapter11/src/Bank/AccountClients.java
+++ ObjOriented_Chapter11/src/Bank/AccountClients.java
@@ -7,7 +7,7 @@ public class AccountClients {
 	double tc;
 	
 	
-public AccountClients(String name,String surname,int tc) {
+public AccountClients(String name,String surname,double tc) {
 	this.name=name;
 	this.surname=surname;
 	this.tc=tc;
